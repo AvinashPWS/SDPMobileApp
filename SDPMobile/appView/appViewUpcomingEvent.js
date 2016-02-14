a88dc391a63e0c21541264c0747489ba82631663
ANARA <Mil12345>
2016-02-14 23:49:02 +0530

    SDP-MASTER : Enabled Upcoming Event List

--- SDPMobile/appView/appViewUpcomingEvent.js
+++ SDPMobile/appView/appViewUpcomingEvent.js
@@ -14,7 +14,13 @@ var appViewUpcomingEvent = {
     },
     responseUpcomingEvents: function (sResponseJSON) {
 
-
+        for (var i in sResponseJSON) {
+            var sEvent = sResponseJSON[i];
+            var sBTag = $('<b></b>').html(sEvent['EVENT_DATETIME']);
+            var sPTag = $('<p></p>').html(sEvent['EVENT_NAME']);
+            var sLiTag = $('<li></li>').html(sBTag).append(sPTag).append($('<br>'));
+            $("#appViewUpcomingEventList").append(sLiTag);
+        }
     }
 
 };
