287242c4eb08a8793a1e27d3ba79d7cf444f0d6e
AAKM <AAKM@PF0395VU.ap.ds.monsanto.com>
2016-02-14 23:17:01 +0530

    Ask Baba Screen

---
+++ SDPMobile/appController/appControllerAskbaba.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var appControllerAskbaba = {
+    requestQuote: function () {
+
+        var sRequestJSON = {
+            "sSP_Name": "LIST_QUOTES"
+        };
+
+
+        appService.invoke("GetData",
+                sRequestJSON,
+                appControllerAskbaba.responseQuote,
+                appControllerAskbaba.errorQuote);
+
+    },
+    responseQuote: function (sRequestJSON, sResponseJSON) {
+        appViewAskbaba.responseQuote(sResponseJSON);
+    },
+    errorQuote: function () {
+
+    }
+};
---
+++ SDPMobile/appView/appViewAskbaba.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+///<reference path='/appView/appConstant.js' />
+
+var appViewAskbaba = {
+    initialize: function () {
+
+    },
+    onClickAskbaba:function (){
+        
+    },
+    responseQuote:function (sResponseJSON){
+        
+    }
+
+};
